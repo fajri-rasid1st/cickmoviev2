636d5a22d1e6899adf77f8300bf9f7134e5ce71e
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2021-06-08 16:57:43 +0800

    Update UI

--- app/src/main/java/com/example/cickmoviev2/ui/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/cickmoviev2/ui/activities/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.cickmoviev2.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.cickmoviev2.R;
 
@@ -12,5 +14,12 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent mainActivity = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(mainActivity);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, 2000);
     }
 }
